20c91d78aa8a0f1f227d48092a571466fff90e78
RiverAndBoat <842714673@qq.com>
2020-09-25 15:16:37 +0800

    remove security

--- src/main/java/com/thoughtworks/demo1/controller/FirstController.java
+++ src/main/java/com/thoughtworks/demo1/controller/FirstController.java
@@ -1,11 +1,16 @@
 package com.thoughtworks.demo1.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class FirstController {
     @GetMapping("/hello")
+    @ResponseStatus(HttpStatus.OK)
     public String getFirstController() {
         return "Hello World";
     }
---
+++ src/test/java/com/thoughtworks/demo1/controller/FirstControllerTest.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.demo1.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FirstController.class)
+@AutoConfigureJsonTesters
+class FirstControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void should_return_hello_world() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk());
+    }
+}
